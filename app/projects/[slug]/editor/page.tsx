4adf346a07baf4f663ce5a5c0f2785267f56e04f
Mahi-Basupally <bhelmahi@gmail.com>
2026-08-26 12:50:47 -0400

    Switch project editor to lot-editor

--- app/projects/[slug]/editor/page.tsx
+++ app/projects/[slug]/editor/page.tsx
@@ -1,6 +1,6 @@
-import PlotEditor from '@/components/plot-editor-v2';
+import LotEditor from '@/components/lot-editor';
 
 export default async function ProjectEditorPage({ params }: { params: Promise<{ slug: string }> }) {
   const { slug } = await params;
-  return <PlotEditor projectSlug={slug} />;
+  return <LotEditor projectSlug={slug} />;
 }
